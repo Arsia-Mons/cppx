d1084682de68564b232ce65569adb331d334a611
Henry Ventura <hvent90@gmail.com>
2026-05-27 09:23:53 -0700

    clean up

--- src-archive/ui/primitives/ListItem.cpp
+++
@@ -1,9 +0,0 @@
-#include "ui/primitives/ListItem.h"
-
-namespace ui {
-
-void ListItem(UiFrameContext &frame, Clay_ElementId id, const std::string &label, bool selected, std::function<void()> onPressed) {
-    Button(frame, id, label, selected ? ButtonVariant::Primary : ButtonVariant::Ghost, std::move(onPressed));
-}
-
-}
--- src-archive/ui/primitives/ListItem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ui/primitives/Button.h"
-
-namespace ui {
-
-void ListItem(UiFrameContext &frame, Clay_ElementId id, const std::string &label, bool selected, std::function<void()> onPressed);
-
-}
